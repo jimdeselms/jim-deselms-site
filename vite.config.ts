a5adb3af6bab363ab97a65ce3e89258fb88b97d2
James DeSelms <jamesdeselms@Jamess-MBP.fios-router.home>
2025-08-02 10:21:26 -0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   build: {
     outDir: 'docs'
   },
-  base: '/jim-deselms-site/' // replace <repo-name> with your GitHub repo name
+  base: './' // Use relative base for GitHub Pages
 })
